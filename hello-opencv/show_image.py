3c75585b4a7c0579860105260c64cc0de633494d
lucl1 <luchenglin@leadcoretech.com>
2017-09-25 09:17:35 +0800

    [add] Hello_OpenCV

---
+++ hello-opencv/show_image.py
@@ -0,0 +1,19 @@
+# -*- coding:gbk -*-
+import cv2
+import numpy as np
+
+img = cv2.imread("imggroup.jpg")
+cv2.namedWindow("image group member")
+cv2.imshow("image group member",img)
+
+img3 = np.random.random((600, 800, 3))
+
+while 1:
+    img3 = np.random.random((600, 800, 3))
+    img3 *= 50
+    img3 = img3.round()
+    cv2.imshow('img',img3)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cv2.destroyAllWindows()
